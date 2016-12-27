95d441a29a44d08c08043b94524d6b90b88cefb3
Landon Renzullo <landonrenzullo@gmail.com>
2016-12-27 12:33:21 -0500

    Update tda2.py

--- tda2.py
+++ tda2.py
@@ -1,6 +1,6 @@
-#Cleaned up collection of funtions from TDA and Betti, with improvements
-
-def c(s,p):
+#cleaned up/optimized collection of functions from Betti and TDA. This collection can currently generate the boundary matrix of 
+#simplices with relations (gluings). TDA 2016-17 Landon Renzullo/Marian Anton CCSU
+def complexify(s,p = 0): #generates the complex of simplex s when p is left as 0. Otherwise a subcomplex of faces with vertices < p never removed, is generated
     if len(s)==1:
         return[]
     else:
@@ -10,10 +10,10 @@ def c(s,p):
             ss=s.copy()
             ss.remove(s[n])
             l.append(ss)
-            l=l+c(ss,i)
+            l=l+complexify(ss,i)
         return l
 
-def isLess(s,t):
+def isLess(s,t): #lexicographical ordering boolean function for simplices
     n=min(len(s),len(t))
     for i in range(0,n):
         if s[i]<t[i]:
@@ -26,7 +26,7 @@ def isLess(s,t):
 
 import random
 
-def MakeLex(l):
+def MakeLex(l): #quicksort implimented with lexicographic ordering that also preferences size of simplices
     if len(l)<=1:
         return l
     p=random.randrange(0,len(l))
@@ -47,7 +47,7 @@ def MakeLex(l):
 
 from sympy import *
 
-def choose(n,k):
+def choose(n,k): #standard combination function
     if 0<=k<=n:
         num=1
         den=1
@@ -59,13 +59,13 @@ def choose(n,k):
     else:
         return 0
 
-def j(gen,n):
+def j(gen,n): #returns the number of n dimensional simplices that will be generated from the generating set gen
     sum=0
     for i in range(0,len(gen)):
         sum=sum+gen[i][0]*choose(gen[i][1]+1,n+1)
     return sum
 
-def par(gen):
+def par(gen): #returns partition generated from generating set gen
     P=[]
     N=[]
     for i in range(0,j(gen,1)):
@@ -78,11 +78,11 @@ def par(gen):
     return P
 
 
-def M(gen):
+def M(gen): #generates the full master list of simplices from gen
     P=par(gen)
     l=[]
     for i in range(0,len(P)):
-        l=l+c(P[i],0)+[P[i]]
+        l=l+complexify(P[i],0)+[P[i]]
     return MakeLex(l)
 
 
@@ -90,7 +90,7 @@ def h(s): #converts a simplice to string and returns its hash value
     str1 = ','.join(str(e) for e in s)
     return hash(str1)
 
-def location(s,x = 'r'):
+def location(s,x = 'r'): #uses hash table H to find quickly find the relative ('r') or absolute ordering of a simplex
     k = h(s)%len(H)
     if type(H[k][0]) == list:
         for i in range(0,len(H[k])):
@@ -105,37 +105,42 @@ def location(s,x = 'r'):
         else:
             return H[k][1]
 
+def setGenData(s,r = []):
+    global master,D,H
+    master.clear()
+    D.clear()
+    master = M(s)
+    D=[zeros(1,j(s,0))]
+    d = s[len(s)-1][1]
+    for i in range(0,d): #initializes boundary matrix D
+        D.append(zeros(j(s,i),j(s,i+1)))
+    H.clear()
+    for i in range(0,2*len(master)): #initializes ordinal hash table H
+        H.append(0)
+    c = 0
+    for i in range(0,len(master)): #fills H with simplex positions
+        n = h(master[i])%len(H)
+        if len(master[i]) > len(master[i-1]):
+            c = 0
+        if H[n] == 0:
+            H[n] = [c,i,master[i],master[i]]
+        elif type(H[n][0]) == list:
+            H[n].append([c,i,master[i],master[i]])
+        else:
+            H[n] = [H[n],[c,i,master[i],master[i]]]
+        c = c+1
+
+    initializeRelations(r)
+    makeBoundary(s)
 
 
-gen=[[2,10]]
-P = par(gen)
-d=gen[len(gen)-1][1]
-global D,H
-D=[zeros(1,j(gen,0))]
-for i in range(0,d):
-    D.append(zeros(j(gen,i),j(gen,i+1)))
-
-
-
-
-master = M(gen)
+global D,H,master
+master = []
+D = []
 H = []
-for i in range(0,2*len(master)):
-    H.append(0)
-c = 0
-for i in range(0,len(master)):
-    n = h(master[i])%len(H)
-    if len(master[i]) > len(master[i-1]):
-        c = 0
-    if H[n] == 0:
-        H[n] = [c,i,master[i]]
-    elif type(H[n][0]) == list:
-        H[n].append([c,i,master[i]])
-    else:
-        H[n] = [H[n],[c,i,master[i]]]
-    c = c+1
 
-def boundarydecomp(s,p):
+
+def boundarydecomp(s,p = 0): #takes simplex s, and recursively calls a rightbound. Will update all boundaries of the COMPLEX of s (s and all faces)
     global D
     if type(s) == int:
         s = [s]
@@ -146,41 +151,66 @@ def boundarydecomp(s,p):
             break
         ss = s.copy()
         ss.remove(s[dim-x])
-        pos = location(ss,'r')
-        if dim-x%2 ==0:
-            m[pos,location(s,'r')] = 1
+        pos = location(lowestOrderRelation(ss),'r')
+        if (dim-x)%2 ==0:
+            m[pos,location(s,'r')] = m[pos,location(s,'r')] + 1
         else:
-            m[pos,location(s,'r')] = -1
+            m[pos,location(s,'r')] = m[pos,location(s,'r')] - 1
     for y in range(p,len(s)):
         if len(s) <= 1:
             break
         ss = s.copy()
         ss.remove(s[dim-y])
-        pos = location(ss,'r')
-        if dim-y%2 ==0:
-            m[pos,location(s,'r')] = 1
+        pos = location(lowestOrderRelation(ss),'r')
+        if (dim-y)%2 == 0:
+            m[pos,location(s,'r')] = m[pos,location(s,'r')] + 1
         else:
-            m[pos,location(s,'r')] = -1
+            m[pos,location(s,'r')] = m[pos,location(s,'r')] - 1
         boundarydecomp(ss,y)
 
-for i in range(0,len(P)):
-    boundarydecomp(P[i],0)
-#print(D)
+def makeBoundary(s):
+    P = par(s)
+    for i in range(0,len(P)):
+        boundarydecomp(P[i],0)
 
-#print(d)
-#print(j(gen,0))
-#print(j(gen,1))
-#print(D[1].shape)
-
-
-
-
-
-
-
-
-#s=[0,1,2,3]
-#t=[4,5,6]
-#l=c(s,0)+[s]+c(t,0)+[t]
-#print(l)
-#print(MakeLex(l))
+def initializeRelations(Rel):#gets the full set of relations, and updates H with relation information for each simplex
+    global H
+    Rel = getAllRelations(Rel)
+    for i in range(0,len(Rel)):
+        s = MakeLex(Rel[i])
+        lor = s[0]
+        for u in range(0,len(s)):
+            k = h(s[u])%len(H)
+            if type(H[k][0]) == list:
+                for x in range(0,len(H[k])):
+                    if H[k][x][2] == s[u]:
+                        H[k][x][3] = lor
+                        break
+            else:
+                H[k][3] = lor
+def lowestOrderRelation(s): #finds the lowest order relation of simplex s, using H
+    k = h(s)%len(H)
+    if type(H[k][0]) == list:
+        for i in range(0,len(H[k])):
+            if H[k][i][2] == s:
+                if H[k][i][3] == s:
+                    return s
+                else:
+                    return lowestOrderRelation(H[k][i][3])
+    else:
+        if H[k][3] == s:
+            return s
+        else:
+            return lowestOrderRelation(H[k][3])
+def getAllRelations(Rel):#generates all lower order relations from a relation set Rel
+    fullRel = []
+    for i in range(0,len(Rel)):
+        lowerRels = []
+        for u in range(0,len(Rel[i])):
+            lowerRels.append(complexify(Rel[i][u],0))
+        for k in range(0,len(lowerRels[0])):
+            newRel = []
+            for x in range(0,len(lowerRels)):
+                newRel.append(lowerRels[x][k])
+            fullRel.append(newRel)
+    return Rel+fullRel
